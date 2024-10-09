c181ae2731ee5dea4e1d5f8bd07bdcb33738ab53
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-10-09 15:18:08 -0300

    feat: establishes connection from apollo server to mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 import resolvers from './resolvers';
 import typeDefs from './typeDefs';
 
 const server = new ApolloServer({ resolvers, typeDefs });
 
-server.listen().then(({ url }) => console.log(url));
+mongoose
+    .connect(process.env.MONGO_DB_URL as string)
+    .then(() => {
+        console.log('MongoDB connected successfully');
+
+        return server.listen({ port: 5000 });
+    })
+    .then((res) => {
+        console.log(`Server is running on port ${res.url}`);
+    });

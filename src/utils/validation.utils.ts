a14f305b4d6141b0e5c418097f5d5579874b640f
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-11-18 18:05:12 -0300

    feat: add error throw using class validator

--- src/utils/validation.utils.ts
+++ src/utils/validation.utils.ts
@@ -1,12 +1,19 @@
 import { validate } from 'class-validator';
 import { plainToInstance } from 'class-transformer';
+import { ApolloError } from 'apollo-server';
 
 async function validateDTO(dto: any, dtoClass: any) {
     const object = plainToInstance(dtoClass, dto);
     const errors = await validate(object);
 
     if (errors.length > 0) {
-        throw new Error('Validation failed!');
+        const formattedErrors = errors.map((error) => {
+            const constraints = error.constraints ? Object.values(error.constraints) : [];
+
+            return constraints.join(', ');
+        });
+
+        throw new ApolloError(formattedErrors.join('; '), 'VALIDATION_ERRORS');
     }
 }
 

7119200d73d100759d98ec8204181568cda7fda6
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-11-18 17:31:37 -0300

    chore: adjust indentation

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -2,9 +2,7 @@ import { join } from 'path';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
 
-const allResolvers = loadFilesSync(
-    join(__dirname, 'modules', '**', 'resolvers.ts'),
-);
+const allResolvers = loadFilesSync(join(__dirname, 'modules', '**', 'resolvers.ts'));
 const allTypes = loadFilesSync(join(__dirname, 'modules', '**', '*.gql'));
 
 const resolvers = mergeResolvers(allResolvers);

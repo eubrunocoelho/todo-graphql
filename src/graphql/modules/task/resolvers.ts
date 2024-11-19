81b8e3980126124867e4cc183abf7e54d6cb2f62
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-11-19 17:08:24 -0300

    chore: update context variable name

--- src/graphql/modules/task/resolvers.ts
+++ src/graphql/modules/task/resolvers.ts
@@ -1,24 +1,24 @@
 const resolvers = {
     Query: {
         async findTask(_, { ID }, context) {
-            return await context.TaskService.findOne(ID);
+            return await context.taskService.findOne(ID);
         },
 
         async allTasks(_, args, context) {
-            return await context.TaskService.findAll();
+            return await context.taskService.findAll();
         },
     },
     Mutation: {
         async createTask(_, { taskInput }, context) {
-            return await context.TaskService.create(taskInput);
+            return await context.taskService.create(taskInput);
         },
 
         async updateTask(_, { ID, taskInput }, context) {
-            return await context.TaskService.update(ID, taskInput);
+            return await context.taskService.update(ID, taskInput);
         },
 
         async deleteTask(_, { ID }, context) {
-            return await context.TaskService.delete(ID);
+            return await context.taskService.delete(ID);
         },
     },
 };
